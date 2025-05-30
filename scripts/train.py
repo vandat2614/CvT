78c0a270b0ac15b2631c6108e209216ee41257c8
Van Datt <dovandat2614@gmail.com>
2025-05-30 23:06:16 +0700

    allow more one optimizer methods

--- scripts/train.py
+++ scripts/train.py
@@ -11,7 +11,6 @@ import torch
 import torch.nn as nn
 
 from torchvision.models import resnet18
-from src.models import ConvolutionalVisionTransformer, ViT
 from src.data import create_data_loaders
 from src.utils import *
 
@@ -49,6 +48,7 @@ def parse_args():
                        help='minimum change in val accuracy to be considered as improvement')
     return parser.parse_args()
 
+
 def train_epoch(model, train_loader, criterion, optimizer, scheduler, device, epoch, logger):
     model.train()
     total_loss = 0
@@ -65,9 +65,10 @@ def train_epoch(model, train_loader, criterion, optimizer, scheduler, device, ep
         loss.backward()
         optimizer.step()
         
-        # Update scheduler
-        scheduler.step() 
-        
+        # Update scheduler only for OneCycleLR
+        if isinstance(scheduler, torch.optim.lr_scheduler.OneCycleLR):
+            scheduler.step()
+
         # Calculate accuracy
         pred = output.argmax(dim=1)
         correct += pred.eq(target).sum().item()
@@ -76,6 +77,9 @@ def train_epoch(model, train_loader, criterion, optimizer, scheduler, device, ep
         total_loss += loss.item()
         current_lr = optimizer.param_groups[0]['lr']
 
+    # Step scheduler
+    if isinstance(scheduler, torch.optim.lr_scheduler.StepLR):
+        scheduler.step()  # StepLR steps every epoch
             
     avg_loss = total_loss / len(train_loader)
     accuracy = 100. * correct / len(train_loader.dataset)
@@ -146,22 +150,12 @@ def main():
 
     # Define loss and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.AdamW(
-        model.parameters(),
-        lr=config['TRAIN']['LR'],
-        weight_decay=config['TRAIN']['WD']
-    )
 
-    # Setup scheduler
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(
-        optimizer,
-        max_lr=config['TRAIN']['LR'],
-        epochs=config['TRAIN']['END_EPOCH'],
-        steps_per_epoch=len(train_loader),
-        pct_start=config['TRAIN']['WARMUP_EPOCHS'] / config['TRAIN']['END_EPOCH'],
-        div_factor=25,
-        final_div_factor=1e4
-    )
+    # Create optimizer
+    optimizer = create_optimizer(config, model.parameters())
+
+    # Create scheduler
+    scheduler = create_scheduler(config, optimizer, train_loader)
 
     # Resume from checkpoint if specified
     start_epoch = config['TRAIN']['BEGIN_EPOCH']
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,7 @@
 from .logger import setup_logging
 from .early_stopping import EarlyStopping
-from .model_builder import create_model
+from .model_factory import create_model
+from .optimizer_factory import create_optimizer, create_scheduler
 
-__all__ = ['setup_logging', 'EarlyStopping', 'create_model']
+__all__ = ['setup_logging', 'EarlyStopping', 'create_model', 
+           'create_optimizer', 'create_scheduler']
